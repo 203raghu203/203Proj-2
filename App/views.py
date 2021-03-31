e266aa5e6dc3864c8981286673af4a60d07d6da2
Raghuram219 <219raghu219@gmail.com>
2021-03-31 10:14:49 +0530

    views setup

--- App/views.py
+++ App/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.forms import StudentForm
 
 def index(request):
-    return HttpResponse("Hellow world")
+    form=StudentForm()
+    if request.method=='POST':
+        form=StudentForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return render(request,'index.html',{'form':form})
+    return render(request,'index.html',{'form':form})
